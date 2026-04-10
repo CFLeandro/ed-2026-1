7400f349072f98818e6c644ed92db1dab0a504b1
Caio Leandro <caiofrele@gmail.com>
2026-04-10 10:49:55 -0300

    aula 03: bubble_sort e partição

---
+++ aulas/aula03/ordenacao.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "ordenacao.h"
+
+void troca (int *a, int *b) {
+    int aux = *a;
+    *a = *b;
+    *b = aux;
+}
+
+void bubble_sort(int v[], int n) {
+    for(int i=0; i < n-1; i++) {
+        for(int j=i+1; j < n; j++) {
+            if(v[i] > v[j]) {
+                troca(&v[i], &v[j]);
+            }
+        }
+    }
+}
+
+void imprimir_vetor(int v[], int n) {
+    print("[");
+    for (int i=0; i < n-1; i++) {
+        printf("%i, ", v[i]);
+    }
+    printf("%i]\n", v[n-1]);
+}
+
+void particionar_vetor(int v[], int inicio, int fim){
+    int pivo = v[fim];
+    int i = inicio -1;
+    for(int j = inicio; j < fim; j++) {
+        if (v[j] <= pivo) {
+            i++;
+            troca(&v[i], &v[j]);
+        }
+    }
+    troca(&v[i + 1], &v[fim]);
+    return i + 1;
+
+}
+
+int quick_sort(int v[], int inicio, int fim) {
+    if(inicio < fim) {
+        int p = particionar_vetor(v, inicio, fim);
+        quick_sort(v, inicio, p - 1);
+        quick_sort(v, p + 1, fim);
+    }
+}
